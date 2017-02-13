645d72f4fcc07075aeb4608778b8b454c262b11a
eugene_pratusevich <e.pratusevich@gmail.com>
2017-02-13 13:06:02 +0300

    Added registration and 1st wave screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,44 +5,72 @@ document.addEventListener("deviceready", function() {
 }, false);
 
 $(document).ready(function() {
-  $('body').on('click', '#open_menu', function() {
-    if ($(this).hasClass('active')) {
-      $('#menu_wrapper').animate({
-        'left': '-1000px'
-      });
-      $(this).removeClass('active');
-    } else {
-      $('#menu_wrapper').animate({
-        'left': '0'
-      });
-      $(this).addClass('active');
-    }
+  $('body').on('touchend', '#open_menu', function() {
+    $('#menu_wrapper').toggleClass('active');
   }).on('change', '.select_type input', function() {
     $(this).parents('label').addClass('checked').siblings('label').removeClass('checked');
-  }).on('click', '.submit_button', function() {
+  }).on('touchend', '.submit_button', function() {
     var form = $(this).parents('form').submit();
-  }).on('click', '.switch_screen', function () {
+  }).on('touchend', '.switch_screen', function () {
     var screen = $(this).data('to');
-    switchTo(screen);
+    var params;
+    if ($(this).data('param')) params = $(this).data('param');
+    switchTo(screen, params);
   }).on('submit', 'form', function(e) {
     e.preventDefault();
   }).on('submit', '#login_form', function(e) {
-    var phone = $(this).find('input[name="phone"]').val().replace(/[^0-9.]/g, '');
+    var phone = $(this).find('input[name="phone"]').val();//.replace(/[^0-9.]/g, '');
     var pwd = $(this).find('input[name="pwd"]').val();
     var data = 'phone=' + phone + '&pwd=' + pwd;
-    switchTo('pet_card');
-    return;
+
     sendRequest('/user/login', data, 'post', function(d) {
-      console.log(d);
+      if (d.result && d.result.token) {
+        window.localStorage.setItem('_token', d.result.token);
+        switchTo('pet_card');
+      } else {
+        console.log(d);
+        alert('Login error');
+      }
     })
+  }).on('submit', '#register_form', function(e) {
+    var phone = $(this).find('input[name="phone"]').val().replace(/[^0-9.]/g, '');
+    var pwd = $(this).find('input[name="pwd"]').val();
+    var fio = $(this).find('input[name="fio"]').val();
+    var data = 'phone=' + phone + '&pwd=' + pwd + '&fio=' + fio;
+    var checkboxes = $(this).find('input[type="checkbox"]');
+
+    if (checkboxes[0].checked && checkboxes[1].checked) {
+      sendRequest('/user/register', data, 'post', function (d) {
+        if (d.result) {
+          switchTo('login');
+        } else {
+          console.log(d);
+          alert('Register error');
+        }
+      });
+    } else {
+      alert('Примите условия пользования приложением');
+    }
   });
 
-  function switchTo(screen) {
+  function switchTo(screen, params) {
     var to_elem = $('.screen_' + screen);
     if (to_elem.hasClass('brown_bg')) $('.content').addClass('brown_bg');
     else $('.content').removeClass('brown_bg');
     if (to_elem.hasClass('no_footer')) $('.footer').hide()
     else $('.footer').show();
+    if (params) {
+      if (typeof params == 'string') {
+        try {
+          params = JSON.parse(params);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      for (var p in params) {
+        to_elem.find('input[name="' + p + '"], select[name="' + p + '"]').val(params[p]);
+      }
+    }
 
     $('.section_wrapper').removeClass('active');
     to_elem.addClass('active');
@@ -54,10 +82,19 @@ $(document).ready(function() {
       data: params,
       type: method,
       success: function(d) {
-        if (typeof callback == 'function') callback(d);
+        var data = d;
+        if (typeof d == 'string') {
+          try {
+            data = JSON.parse(d);
+          } catch (e) {
+            console.log(e);
+          }
+        }
+        if (typeof callback == 'function') callback(data);
       },
       error: function (a,b,c) {
         console.log(a,b,c);
+        alert(c);
       }
     });
   }
